6a9d4f244b490c8a30aae632aecff98879f2a73e
Bandar-Zuhair <my.github.websites@gmail.com>
2024-12-07 19:10:10 +0700

    .

--- seef-travel.js
+++ seef-travel.js
@@ -423,7 +423,7 @@ function openFullScreenImage(src, text) {
     const whatsappButton = document.createElement('a');
     whatsappButton.className = 'whatsapp_button';
     whatsappButton.innerText = 'إرسال هذا العرض';
-    whatsappButton.href = `https://wa.me/+97337111535?text=طلب%20حجز%20هذا%20العرض:%0Ahttps://mohammed-website.github.io/seeftravel/${encodeURIComponent(src)}`;
+    whatsappButton.href = `https://wa.me/+966551243752?text=طلب%20حجز%20هذا%20العرض:%0Ahttps://mohammed-website.github.io/seeftravel/${encodeURIComponent(src)}`;
     fullScreenDiv.appendChild(whatsappButton);
 
     // Close on background click
@@ -706,7 +706,7 @@ openWhatsAppNumber = function () {
 
     insertNewClick('alseef.com');
 
-    const whatsappNumber = "+97337111535";
+    const whatsappNumber = "+966551243752";
     const message = encodeURIComponent('سلام عليكم ورحمة الله وبركاته'); // Optional pre-filled message
     const url = `https://wa.me/${whatsappNumber}?text=${message}`;
     window.open(url, "_blank"); // Opens in a new tab
